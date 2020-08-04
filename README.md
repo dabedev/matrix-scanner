fdac08362527d7da955b125caa2468d0d995651a
DannyyDev <42282680+DannyyDev@users.noreply.github.com>
2020-08-04 16:16:57 -0400

    First commit.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # matrix-scanner
- C++ element position scanner algorithm
+C++ element position scanner algorithm.
+
+## Developed with:
+> - C++
+
+## Discord Server
+<p align="center">
+  <a href="https://discord.gg/MF6EGyr"><img src="https://discordapp.com/api/guilds/571872011551244298/widget.png?style=banner2" alt="Discord server"></a>
+</p>
---
+++ algorithm.cpp
@@ -0,0 +1,92 @@
+#include "iostream"
+#include "stdio.h"
+#include "string"
+
+#define Max 3
+
+using namespace std;
+
+int main()
+{
+    int arr[Max][Max];
+    int arr2[Max][Max];
+    char superior;
+    char inferior;
+    int insertnum;
+    for(int i = 0; i < Max; i++)
+    {
+            for(int x = 0; x < Max; x++)
+    {
+        cout<<"Matriz["<<i<<"]"<<"["<<x<<"] = ";
+        cin>>insertnum;
+        arr[i][x] = insertnum;
+    }
+    }
+    cout<<"Resultado de la matriz:"<<endl;
+    for(int i = 0; i < Max; i++)
+    {
+            for(int x = 0; x < Max; x++)
+    {
+        if (x == Max - 1) {
+            cout<<arr[i][x]<<endl;
+        } else {
+        cout<<arr[i][x]<<" ";
+        }
+    }
+    }
+    cout<<"Elementos en la diagonal:"<<endl;
+        for(int i = 0; i < Max; i++)
+    {
+        cout<<arr[i][i]<<" ";
+        if (i == Max - 1) cout<<"\n";
+    }
+    printf("\n");
+   cout<<"Elementos en la parte superior de la diagonal:"<<endl;
+    for(int i = 0; i < Max; i++)
+    {
+            for(int x = 0; x < Max; x++)
+    {
+        if (x <= i) continue;
+        cout<<arr[i][x];
+        superior=superior + (char)arr[i][x];
+    }
+    }
+    printf("\n");
+   cout<<"Elementos en la parte inferior de la diagonal:"<<endl;
+        for(int i = 0; i < Max; i++)
+    {
+            for(int x = 0; x < Max; x++)
+    {
+        if (x >= i) continue;
+        cout<<arr[i][x];
+        inferior=inferior + (char)arr[i][x];
+    }
+    }
+    /*printf("\n");
+    cout<<superior<<"\n"<<inferior;
+    printf("\n");
+    if (inferior == superior) {
+    for(int i = 0; i < Max; i++)
+    {
+            for(int x = 0; x < Max; x++)
+    {
+        arr2[i][x] = arr[Max-i][Max-x];
+    }
+    }
+        cout<<"Resultado de la matriz invertida:"<<endl;
+    for(int i = 0; i < Max; i++)
+    {
+            for(int x = 0; x < Max; x++)
+    {
+        if (x == Max - 1) {
+            cout<<arr[i][x]<<endl;
+        } else {
+        cout<<arr[i][x]<<" ";
+        }
+    }
+    }
+    }
+    else return false;*/
+
+    return 0;
+}
